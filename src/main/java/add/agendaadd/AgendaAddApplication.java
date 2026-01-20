9bd7ca37b386af89327c46ab553b7fe421c0b634
Fabricio3916 <fabriciopassos3916@gmail.com>
2026-01-20 15:36:37 -0300

    Primeiro commit

---
+++ src/main/java/add/agendaadd/AgendaAddApplication.java
@@ -0,0 +1,13 @@
+package add.agendaadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaAddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaAddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/add/agendaadd/AgendaAddApplicationTests.java
@@ -0,0 +1,13 @@
+package add.agendaadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaAddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
